274b27ccb7d569dca3b9c47f0930b9878e846642
Your Name <you@example.com>
2023-02-21 11:50:17 -0800

    Task done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - adds two integars together
+ * @a: the first number
+ * @b: the second number
+ * Return: the addition of fboth
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
