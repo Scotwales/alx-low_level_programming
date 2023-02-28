2dcaeef312219c8192552df39ca1030eb7133566
Your Name <you@example.com>
2023-02-28 02:54:59 -0800

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - a function that resets int value back to 98
+ * and also corrcts it
+ * @n: input
+ * Return: n
+ */
+int reset_to_98(int *n)
+{
+	*n = 98;
+}
