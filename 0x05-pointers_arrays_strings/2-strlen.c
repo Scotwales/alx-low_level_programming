3c829ea74c1a0f40d620801ed20696f8ede33508
Your Name <you@example.com>
2023-02-28 16:09:40 -0800

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,15 +2,17 @@
 /**
  * _strlen - a function that returns the length of a string
  * @s: value of string
- * Return: s
+ * Return: length
  */
 int _strlen(char *s)
 {
-	char *str;
-	int len;
-	
-	str = "My first strlen!";
-	len = _strlen(str);
-	printf("%d\n", len);
-	return (0);
+	int long1 = 0;
+
+	while (*s != '\0')
+	{
+		long1++;
+		s++;
+	}
+
+	return (long1);
 }
